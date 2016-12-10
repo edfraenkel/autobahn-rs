7625e5e6c7a168cba4013d000367fc24b64efc3f
Daniel Fraenkel <e.d.fraenkel@gmail.com>
2016-12-10 17:20:44 +0100

    implemented wamp message serialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+#![feature(proc_macro, try_from, slice_patterns)]
+#![feature(custom_derive, plugin)]
+
+
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+extern crate serde;
+
+#[macro_use] extern crate enum_primitive;
+extern crate num;
+
+mod serialization;
+
 #[cfg(test)]
 mod tests {
     #[test]
